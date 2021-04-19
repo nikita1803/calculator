d7ebfbe80dd19cb2d0ae3e69cca491f62d5e196a
nikita1803 <rainikita595@gmail.com>
2021-04-19 19:47:20 +0530

    Added substraction of 2 number

--- calculator.sh
+++ calculator.sh
@@ -1,5 +1,9 @@
 echo "Welcome"
-num1=20
-num2=30
+num1=30
+num2=20
 add=$(($num1+$num2))
-echo $add
+echo "sum of two number"$add
+num1=40
+num2=20
+sub=$(($num1-$num2))
+echo "substraction of two number"$sub
